0906d2afb7d5f2a6925e578d514fb3205579c445
Akhil Tandon <akhil.tandon@gmail.com>
2012-02-25 13:45:53 -0500

    get and set methods

--- Code/src/Player.java
+++ Code/src/Player.java
@@ -4,6 +4,32 @@ public class Player {
 	private String name;
 	private int wealth;
 	private double popularity;
+	public String partyname;
+	
+	public int getWealth(){
+		return wealth;
+	}
+	
+	public void setWealth(int i){
+		wealth=i;
+	}
+	
+	public double getPopularity(){
+		return popularity;
+	}
+	
+	public void setPopularity(double d){
+		popularity=d;
+	}
+	
+	public String getPartyName(){
+		return partyname;
+	}
+	
+	public void setPartyName(String s){
+		partyname=s;
+	}
+	
 	
 	public int getInfluence(){
 		// TODO add influence calculator
