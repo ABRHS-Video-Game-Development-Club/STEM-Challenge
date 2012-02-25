d3d6e9e635c35d7e658065b6502e13c3a82fc8f4
Akhil Tandon <akhil.tandon@gmail.com>
2012-02-25 13:34:39 -0500

    double

--- Code/src/Player.java
+++ Code/src/Player.java
@@ -3,7 +3,7 @@ public class Player {
 	
 	private String name;
 	private int wealth;
-	private int popularity;
+	private double popularity;
 	
 	public int getInfluence(){
 		// TODO add influence calculator
