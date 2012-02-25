9510b2e784695ce1c567b496023694160aa67def
Andrew <dev@andrewdai.co>
2012-02-25 13:27:17 -0500

    Java Class for the player and their stats

---
+++ Code/src/Player.java
@@ -0,0 +1,12 @@
+
+public class Player {
+	
+	private String name;
+	private int wealth;
+	private int popularity;
+	
+	public int getInfluence(){
+		// TODO add influence calculator
+		return 0;
+	}
+}
